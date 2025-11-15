640ecab36bee8b44f55d6489437adb4e05d81386
kaguangieliud56 <kaguangieliud56@gmail.com>
2025-11-15 20:57:55 +0300

    complete all garden management tasks

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,21 @@
-const temperature = 90;
-const timeOfDay = "morning";
+
+
+const temp = 90;
+if (temp > 80) {
+    console.log("Watering on");
+} else {
+    console.log("Watering off");
+}
+const timeOfDay = "morning"
+if (timeOfDay === "evening" || timeOfDay == "night") {
+    console.log("Lights on");
+}else {
+        console.log("Lights off");
+}
+
 let soilMoisture = 30;
+while (soilMoisture < 40){
+    console.log(soilMoisture);
+    soilMoisture += 5;
+}
+console.log(40);
